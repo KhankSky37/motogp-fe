a90ec2ad1089d1d4e6f0fc130e8add4554e4bef3
KhankSky <pkhanhsnd037@gmail.com>
2025-04-22 08:46:19 +0700

    feat: home background

--- motogp_f--/src/pages/Home.jsx
+++ motogp_f--/src/pages/Home.jsx
@@ -5,10 +5,11 @@ import {Button} from "antd"; // Import hình ảnh từ thư mục assets
 const Home = () => {
   return (
     <div
-      className="min-h-[812px] w-full bg-cover bg-center flex items-end justify-start reveal-bg"
+      className="min-h-[812px] w-full bg-cover bg-center flex items-end justify-start bg-gradient-to-b from-transparent to-black"
       style={{ backgroundImage: `url(${bgHome})` }}
     >
-      <div className={"text-white p-12 space-y-6"}>
+      <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black h-full w-full"></div>
+      <div className={"relative text-white p-12 space-y-6"}>
         <div className={"text-xl"}>MotoGP™</div>
         <div className={"text-6xl font-extrabold w-3/5"}>Aprilia provide update on Martin's recovery</div>
         <Button className={"bg-red-600"} >READ NOW</Button>
