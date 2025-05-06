4d9e4c270deb77c6f8fe52aeec765e5250071f2c
Trieu Xuan Huy <trieuxuanuhuytq3105@gmail.com>
2025-05-06 09:37:12 +0700

    update bg

--- motogp_f--/src/layouts/DefaultLayout.jsx
+++ motogp_f--/src/layouts/DefaultLayout.jsx
@@ -1,72 +1,74 @@
-import React from 'react';
-import {Breadcrumb, Layout, Menu, theme} from 'antd';
-import {Outlet} from "react-router-dom";
+import React from "react";
+import { Breadcrumb, Layout, Menu, theme } from "antd";
+import { Outlet } from "react-router-dom";
 import motogp from "../assets/motogp1.jpg";
 
-const {Header, Content, Footer} = Layout;
-
+const { Header, Content, Footer } = Layout;
 
 const DefaultLayout = () => {
-const items =[
-  {
-    key:'calendar',
-    label:'calendar',
-  },
-  {
-    key:'Results&Standings',
-    label:'Results & Standings',
-  },
-  {
-    key:'Riders&Teams',
-    label:'Riders & Teams',
-  },
-  {
-    key:'VideoPass',
-    label:'VideoPass',
-  },
-  {
-    key:'Videos',
-    label:'Videos',
-  },
-  {
-    key:'News',
-    label:'News',
-  },
-]
+  const items = [
+    {
+      key: "calendar",
+      label: "calendar",
+    },
+    {
+      key: "Results&Standings",
+      label: "Results & Standings",
+    },
+    {
+      key: "Riders&Teams",
+      label: "Riders & Teams",
+    },
+    {
+      key: "VideoPass",
+      label: "VideoPass",
+    },
+    {
+      key: "Videos",
+      label: "Videos",
+    },
+    {
+      key: "News",
+      label: "News",
+    },
+  ];
   const {
-    token: {colorBgContainer, borderRadiusLG},
+    token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
   return (
     <Layout>
       <Header
         style={{
-          position: 'sticky',
+          position: "sticky",
           top: 0,
-          zIndex: 1,
-          width: '100%',
-          display: 'flex',
-          alignItems: 'center',
+          zIndex: 999,
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
         }}
         className={"bg-black"}
       >
-
-        <img src={motogp} alt="MotoGP Logo" style={{height: 40, marginRight: 16, width: 200}}/>
+        <img
+          src={motogp}
+          alt="MotoGP Logo"
+          style={{ height: 40, marginRight: 16, width: 200 }}
+        />
         <Menu
           theme="dark"
           mode="horizontal"
-          defaultSelectedKeys={['2']}
+          defaultSelectedKeys={["2"]}
           items={items}
-          style={{flex: 1, minWidth: 0}}
+          style={{ flex: 1, minWidth: 0 }}
           className="text-white font-medium bg-black"
         />
 
         <div className={"text-[#e5e7eb] divide-x-2"}>
-          <span className={'px-2'}>Login</span>
-          <span className={'px-2'}>Register</span>
+          <span className={"px-2"}>Login</span>
+          <span className={"px-2"}>Register</span>
         </div>
       </Header>
       <Content
-        // style={{padding: '0 48px'}}
+      // style={{padding: '0 48px'}}
       >
         {/*<Breadcrumb style={{margin: '16px 0'}}>*/}
         {/*  <Breadcrumb.Item>Home</Breadcrumb.Item>*/}
@@ -84,11 +86,11 @@ const items =[
         {/*>*/}
         {/*  Content*/}
         {/*</div>*/}
-        <Outlet/>
+        <Outlet />
       </Content>
       <Footer className={"px-12 py-4 border-t"}>
-        ©{new Date().getFullYear()} Dorna Sports SL. All rights reserved. All trademarks are the property of their
-        respective owners.
+        ©{new Date().getFullYear()} Dorna Sports SL. All rights reserved. All
+        trademarks are the property of their respective owners.
       </Footer>
     </Layout>
   );
--- motogp_f--/src/pages/user/Home.jsx
+++ motogp_f--/src/pages/user/Home.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import bgHome from '../../assets/bg_home.jpg';
-import {Button} from "antd"; // Import hình ảnh từ thư mục assets
+import React from "react";
+import bgHome from "../../assets/bg_home.jpg";
+import { Button } from "antd"; // Import hình ảnh từ thư mục assets
 
 const Home = () => {
   return (
     <div
-      className="min-h-[812px] w-full bg-cover bg-center flex items-end justify-start bg-gradient-to-b from-transparent to-black"
+      className="relative flex justify-start h-[600px] bg-cover bg-center"
       style={{ backgroundImage: `url(${bgHome})` }}
     >
+      <div className="absolute inset-0 bg-black opacity-40"></div>
+      <div className="absolute inset-x-0 bottom-0 h-[60%] bg-gradient-to-b to-black from-transparent"></div>
+      <div className="absolute inset-y-0 left-0 w-[30%] bg-gradient-to-l to-black from-transparent"></div>
 
-      <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black h-full w-full"></div>
-      <div className={"relative text-white p-12 space-y-6"}>
-        <div className={"text-xl"}>MotoGP™</div>
-        <div className={"text-6xl font-extrabold w-3/5"}>Aprilia provide update on Martin's recovery</div>
-        <Button className={"bg-red-600"} >READ NOW</Button>
+      <div className="absolute inset-x-16 text-white top-1/2">
+        <h1 className="text-4xl font-bold mb-4">Welcome to MotoGP</h1>
+        <p className="text-lg mb-8">
+          Experience the thrill of MotoGP like never before!
+        </p>
+        <Button type="primary" size="large">
+          Get Started
+        </Button>
       </div>
     </div>
   );
